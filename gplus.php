daa79041474a58ea2656358750d7e271c4be5999
Luka Pušić <pusic93@gmail.com>
2011-11-16 23:20:32 +0100

    debug false

--- gplus.php
+++ gplus.php
@@ -24,7 +24,7 @@ $cookies = 'cookie.txt';
 $sleeptime = 0;
 $uagent = 'Mozilla/4.0 (compatible; MSIE 5.0; S60/3.0 NokiaN73-1/2.0(2.0617.0.0.7) Profile/MIDP-2.0 Configuration/CLDC-1.1)';
 $pc_uagent = 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1';
-$debug = TRUE;
+$debug = FALSE;
 
 function tidy($str) {
     return rtrim($str, "&");
